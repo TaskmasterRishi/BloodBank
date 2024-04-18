43f0448724349c057fcecc1a2d003e6c903bf8d2
Rishi Patodiya <rishipatodiya12@gmail.com>
2024-04-18 16:06:58 +0530

    Responsive Profile Page

--- script/donorPro.js
+++ script/donorPro.js
@@ -5,3 +5,13 @@ function uploadFile() {
 function handleFileChange(event) {
     document.getElementById("uploadForm").submit();
 }
+
+function toggleEditForm() {
+    var editForm = document.querySelector('.editForm');
+    if (editForm.style.transform === 'translateX(-200%)' || editForm.style.transform === '') {
+        editForm.style.transform = 'translateX(0%)';
+    } else {
+        editForm.style.transform = 'translateX(-200%)';
+    }
+    document.getElementById("menuToggle").querySelector("input[type='checkbox']").checked = false;
+}
